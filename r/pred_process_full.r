068f95034c63c8d853d606bcf61c196b24c370ab
Andria Dawson <andria.dawson@gmail.com>
2015-05-08 09:27:34 -0600

    Getting pred_process_full to work with read_stabin

--- r/pred_process_full.r
+++ r/pred_process_full.r
@@ -65,9 +65,13 @@ if (!file.exists(paste0('output/', suff_fit,'.rdata'))){
 #   rm(fit)
   fname   = sprintf('output/%s.bin', suff_fit)
   object  = read_stanbin(fname)
-  samples = cbind(object$samples[,5:ncol(object$samples)], object$samples[,1])
-  post = array(0, c(nrow(samples), 1, ncol(samples)))
+#   samples = cbind(object$samples[,5:ncol(object$samples)], object$samples[,1])
+#post = array(0, c(nrow(object$samples), 1, ncol(object$samples)-5))   
+  samples = data.frame(object$samples[,5:ncol(object$samples)], object$samples[,1]))
+#   colnames(samples) = colnames(object$samples cbind(colnames(object$samples[,5:ncol(object$samples)]), name(object$samples[,1])))
+  post = array(0, c(nrow(samples), 1, ncol(samples)))   
   post[,1,] = samples 
+  dimnames(post)[[3]] = colnames(samples)
 } else {
   load(paste0('output/', suff_fit,'.rdata'))
 }
