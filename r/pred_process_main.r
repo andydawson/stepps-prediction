aada7693db7adc6146fbcae54e8888b92394e6aa
Andria Dawson <andria.dawson@gmail.com>
2015-05-13 10:43:26 -0600

    process_main update

--- r/pred_process_main.r
+++ r/pred_process_main.r
@@ -1,8 +1,8 @@
 PL = list(suff_dat = '12taxa_699cells_120knots_0to2000ypb_KW_KGAMMA_PL_umw_3by_v0.3',
-          suff_fit = '12taxa_699cells_120knots_0to2000ypb_KW_KGAMMA_PL_umw_3by_v0.3_tmp')
+          suff_fit = '12taxa_699cells_120knots_0to2000ypb_KW_KGAMMA_PL_umw_3by_v0.3')
 
 G  = list(suff_dat = '12taxa_699cells_120knots_0to2000ypb_KW_KGAMMA_G_umw_3by_v0.3', 
-          suff_fit = '12taxa_699cells_120knots_0to2000ypb_KW_KGAMMA_G_umw_3by_v0.3_tmp')
+          suff_fit = '12taxa_699cells_120knots_0to2000ypb_KW_KGAMMA_G_umw_3by_v0.3')
 
 runs = list(PL, G)
 
