204feada12e1efd56381ea0b96aaf1cd7202f2e5
Andria Dawson <andria.dawson@gmail.com>
2015-05-19 10:11:09 -0600

    Revising processing script

--- r/pred_process_full_test.r
+++ r/pred_process_full_test.r
@@ -9,40 +9,10 @@ source('r/utils/pred_plot_funs.r')
 source('r/utils/pred_helper_funs.r')
 source('r/read_stanbin.r')
 
-# suff_dat = '12taxa_457cells_77knots_0to2000ypb_umwE_3by_v0.3'
-# suff_fit = '12taxa_457cells_77knots_0to2000ypb_umwE_3by_od_mpp_full'
-# suff_dat = '12taxa_459cells_77knots_0to2000ypb_umwW_3by_v0.3'
-# suff_fit = '12taxa_459cells_77knots_0to2000ypb_umwW_3by_od_mpp_full'
-# suff_dat = '12taxa_459cells_77knots_0to2000ypb_umwW_3by_v0.3'
-# suff_fit = '12taxa_459cells_77knots_0to2000ypb_umwW_3by_mpp_full_nug_mu0'
-# suff_dat = '12taxa_457cells_77knots_0to2000ypb_umwE_3by_v0.3'
-# suff_fit = '12taxa_457cells_77knots_0to2000ypb_umwE_3by_mpp_full_nug_mu0'
-
-# new output scaled res
-# suff_dat = 'pred_data_12taxa_387cells_66knots_0to2000ypb_umwE_3by_v0.3'
-# suff_fit = '12taxa_387cells_66knots_0to2000ypb_umwE_3by_od_mpp_full_nug_mu0_res'
-# 
-# suff_fit = '12taxa_459cells_78knots_0to2000ypb_umwW_3by_od_mpp_full_nug_mu0_res'
-
-# suff_dat = '12taxa_699cells_120knots_0to2000ypb_umw_3by_v0.3'
-# suff_fit = '12taxa_699cells_120knots_0to2000ypb_umw_3by_od_mpp_full_nug_mu0_res'
-
-# # suff_dat = '12taxa_699cells_120knots_0to2000ypb_PL_umw_3by_v0.3'
-# # suff_fit = '12taxa_699cells_120knots_0to2000ypb_PL_umw_3by_tmp'
-# 
-# suff_dat = '12taxa_699cells_120knots_0to2000ypb_G_umw_3by_v0.3'
-# suff_fit = '12taxa_699cells_120knots_0to2000ypb_G_umw_3by_tmp'
-
 # where to put the figures
 subDir <- paste("figures/", suff_fit, sep='')
 save_plots = TRUE
 suff = ''
-
-# od         = TRUE
-# bt         = TRUE
-# mpp        = FALSE
-# mut        = FALSE
-# save_plots = TRUE
 
 mu0        = TRUE
 od         = TRUE
@@ -108,23 +78,21 @@ adj_t = adj$adj_t
 ####################################################################################################
 # process_out = build_props_full(post, rho, eta, T, K, d, d_inter, d_knots, od, mpp, mu0)
 process_out = build_r(post, rho, eta, T, K, d, d_inter, d_knots, od, mpp, mu0)
-# save(process_out, file='r/pred/dump/process_out.rdata')
+save(process_out, file=paste0(subDir, 'process_out.rdata'))
 # rm(post)
 
-
-# M = diag(N*T)
-# M = M - P
-# 
-# foo = M %*% Halpha[,1,1]
-# 
-# # Halpha
-
-# mu_g   = process_out$mu_g
-r_pred = process_out$r
-g      = process_out$g
-# Halpha_t = process_out$Halpha_t
-# Halpha_s = process_out$Halpha_s
+# r_pred = process_out$r
+# g      = process_out$g
 # rm(process_out)
+
+
+process_mean = build_mu_g(post, rho, eta, T, K, d, d_inter, d_knots, od, mpp, mu0, res)
+
+mu_g     = process_mean$mu_g
+Halpha_t = process_mean$Halpha_t
+Halpha_s = process_mean$Halpha_s
+
+save(process_mean, file=paste0(subDir, 'process_mean.rdata'))
 
 # niter = dim(post[,1,])[1]
 # mean_Halpha_t = array(NA, dim=c(W, T-1, niter))
@@ -227,159 +195,66 @@ g      = process_out$g
 # dev.off()
 #######################################
 
-# trace_plot_process(mu_g, suff='mu_g', save_plots=save_plots)
-trace_plot_process(r_pred, suff='r', save_plots=save_plots)
-trace_plot_process(g, suff='g', save_plots=save_plots)
-
-
-r_mean = matrix(NA, nrow=N*T, ncol=K)
-g_mean = matrix(NA, nrow=N*T, ncol=W)
-niter = dim(r_pred)[3]
-
-for (i in 1:(N*T)){
-  r_mean[i,] = rowSums(r_pred[i,,])/niter
-  g_mean[i,] = rowSums(g[i,,])/niter
-}
-
-rm(g)
-rm(r_pred)
-
-
-limits = get_limits(centers_pls)
-####################################################################################################
-# chunk: plot predicted distributions
-####################################################################################################
-suff1=paste(suff_fit, '_props', sep='')
-
-# plot_pred_maps(r_mean, centers_veg, taxa=taxa, ages, N, K, T, thresh=0.5, limits, type='prop', suff=suff1,  save_plots=save_plots)
-
-suff1.1=paste(suff_fit, '_props_select', sep='')
-plot_pred_maps_select(r_mean, centers_veg, taxa=taxa, ages, N, K, T, thresh=0.5, limits, type='prop', suff=suff1.1,  save_plots=save_plots)
-
-breaks = c(0, 0.01, 0.05, 0.10, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 1)
-# p_binned <- plot_pred_maps_binned(r_mean, centers_veg, breaks, taxa, ages, N, K, T, limits, suff=suff1, save_plots=save_plots)
-p_binned <- pred_maps_binned_select(r_mean, centers_veg, breaks, taxa, ages, N, K, T, limits, suff1, save_plots, fpath=subDir)
-####################################################################################################
-# chunk: predicted process maps
-####################################################################################################
-suff2=paste(suff_fit, '_process', sep='')
-
-plot_pred_maps(g_mean, centers_veg, taxa=taxa, ages, N, K-1, T, thresh=NA, limits, type='process', suff=suff2,  save_plots=save_plots)
-
-####################################################################################################
-# chunk: plot observed proportions
-####################################################################################################
-suff3=paste(suff_fit, '_data', sep='')
-
-plot_data_maps(y_veg, centers=centers_pls, taxa=taxa, ages, N_pls, K, T, thresh=0.5, limits, suff=suff3, save_plots=save_plots)
-
-suff4=paste(suff_fit, '_data_binned', sep='')
-plot_data_maps_binned(y_veg, centers=centers_pls, taxa=taxa, ages, N_pls, K, T, breaks, limits, suff=suff4, save_plots=save_plots)
-
-
-# N_cores and centers_polU broken for split domain data....
-idx.keep  = c(1,2,length(ages)/2,T)
-# plot_core_locations(y, centers_polU, centers_pls, ages, limits)
-plot_core_locations_select(y, centers_pol, centers_pls, ages, idx.keep, limits, fpat=subDir)
-
-
-# plot5<-grid.arrange(arrangeGrob(c, p_binned, nrow=1), 
-#                      widths=c(10))
-# plot5<-grid.arrange(c, p_binned, heights=c(10,10), nrow=1)
-
-
-# 
-# # objects are c, d, p_binned
-# # Get the widths
-# g <- ggplotGrob(p_binned)
-# keep <- !grepl("strip-top", g$layout$name)
-# g$grobs <- g$grobs[keep]
-# g$layout <- g$layout[keep, ]
-# 
-# # gA <- ggplot_gtable(ggplot_build(p_binned))
-# # gB <- ggplot_gtable(ggplot_build(c))
-# 
-# c_grob <- ggplotGrob(c)
-# c_grob$heights <- g$heights
-# 
-# # gtable_show_layout(g)
-# gf <- gtable_add_cols(g, unit(5, "cm"), pos=0)
-# gf <- gtable_add_cols(g, unit(0.25, "lines"), pos=0)
-# gf <- gtable_add_grob(gf, c_grob,
-#                      t = 3, l=1, b=nrow(g), r=1)
-# g <- gtable_add_rows(g, unit(8, "cm"), pos=0)
-# # g <- gtable_add_rows(g, g$heights[1], pos=0)
-# g <- gtable_add_grob(g, ggplotGrob(d),
-#                      t = 1, l=2, b=1, r=ncol(g)-2)
-# grid.newpage()
-# pdf('figures/pred/test_gtable.pdf', width=12, height=16)
-# grid.draw(g)
-# dev.off()
-# # 
+# # trace_plot_process(mu_g, suff='mu_g', save_plots=save_plots)
+# trace_plot_process(r_pred, suff='r', save_plots=save_plots)
+# trace_plot_process(g, suff='g', save_plots=save_plots)
 # 
 # 
-# ng = nullGrob()
-# g <- gtable(widths = unit(c(5, 7), "in"),  # need lcm(3,2)=6 for the matrix rows
-#             heights = unit(c(5, 7), "in"))
-# gtable_show_layout(g)
+# r_mean = matrix(NA, nrow=N*T, ncol=K)
+# g_mean = matrix(NA, nrow=N*T, ncol=W)
+# niter = dim(r_pred)[3]
 # 
-# # # Add a grob:
-# # rect <- rectGrob(gp = gpar(fill = "black"))
-# # a <- gtable_add_grob(a, rect, 1, 1)
-# # a
-# # plot(a)
+# for (i in 1:(N*T)){
+#   r_mean[i,] = rowSums(r_pred[i,,])/niter
+#   g_mean[i,] = rowSums(g[i,,])/niter
+# }
 # 
-# g<- gtable_add_grob(g, c_grob, 2, 1)
-# g<- gtable_add_grob(g, ggplotGrob(d), 1, 2)
-# g<- gtable_add_grob(g, ggplotGrob(p_binned), 2, 2)
-# 
-# grid.newpage()
-# grid.draw(g)
-
-
-# x1 <- freeze_panels(d, draw=TRUE, width=unit(1,"in"),height=unit(1,"in"))
-# x2 <- freeze_panels(c, draw=TRUE, width=unit(1,"in"),height=unit(1,"in"))
-# x3 <- freeze_panels(p_binned, draw=TRUE, width=unit(1,"in"),height=unit(1,"in"))
+# rm(g)
+# rm(r_pred)
 # 
 # 
+# limits = get_limits(centers_pls)
+# ####################################################################################################
+# # chunk: plot predicted distributions
+# ####################################################################################################
+# suff1=paste(suff_fit, '_props', sep='')
 # 
+# # plot_pred_maps(r_mean, centers_veg, taxa=taxa, ages, N, K, T, thresh=0.5, limits, type='prop', suff=suff1,  save_plots=save_plots)
 # 
+# suff1.1=paste(suff_fit, '_props_select', sep='')
+# plot_pred_maps_select(r_mean, centers_veg, taxa=taxa, ages, N, K, T, thresh=0.5, limits, type='prop', suff=suff1.1,  save_plots=save_plots)
 # 
-# grid.arrange(arrangeGrob(ng, x1, nrow=1, widths=c(0.5,3.5)),
-#              arrangeGrob(x2, x3, nrow=1, widths=c(0.5,3.5)),
-#              nrow=2)
+# breaks = c(0, 0.01, 0.05, 0.10, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 1)
+# # p_binned <- plot_pred_maps_binned(r_mean, centers_veg, breaks, taxa, ages, N, K, T, limits, suff=suff1, save_plots=save_plots)
+# p_binned <- pred_maps_binned_select(r_mean, centers_veg, breaks, taxa, ages, N, K, T, limits, suff1, save_plots, fpath=subDir)
 # 
-# grid.arrange(ng, x1, x2, x3, nrow=2, widths=c(0.5,3.5))
-# 
-# 
-# library(grid)
-# ht = 3
-# grid.newpage()
-# pushViewport(viewport(layout = grid.layout(2, 2, heights = unit(c(ht, length(ages*ht)), "null"))))
-# grid.rect()
-# # grid.text("title of this panel", vp = viewport(layout.pos.row = 1, layout.pos.col = 1:2))
-# print(d, vp = viewport(layout.pos.row = 1, layout.pos.col = 2))
-# print(c, vp = viewport(layout.pos.row = 2, layout.pos.col = 1))
-# print(p_binned, vp = viewport(layout.pos.row = 2, layout.pos.col = 2))
-# 
-# w = 5
-# grid.newpage()
-# pushViewport(viewport(width=10, height=10, default.units="in")); 
-# #gt = grobTree(rectGrob(), circleGrob()) 
-# grid.arrange(c, p_binned, nrow=1, newpage=FALSE,
-#              widths = unit(c(w, w*7), "null"))
-# upViewport()
-# 
-# 
-# top <- arrangeGrob(r, r, r, r, nrow=2)
-# 
-# grid.arrange(top, r, ncol=1, main = "Main Title", heights=c(2, 1))
-# 
-# grid.newpage()
-# align_plots(c,p_binned)
+# # FIX THIS SO it is ONLY select locations...
 # # ####################################################################################################
-# # # chunk: plot predicted and observed proportions in same frame
+# # # chunk: predicted process maps
 # # ####################################################################################################
-# # suff4=paste(suff, '_compare', sep='')
+# # suff2=paste(suff_fit, '_process', sep='')
 # # 
-# # plot_both_maps(r_mean, y_veg, centers=centers_pls, taxa=taxa, ages, N, K, T, thresh=1, suff=suff3, save_plots=save_plots)
+# # plot_pred_maps(g_mean, centers_veg, taxa=taxa, ages, N, K-1, T, thresh=NA, limits, type='process', suff=suff2,  save_plots=save_plots)
+# 
+# ####################################################################################################
+# # chunk: plot observed proportions
+# ####################################################################################################
+# suff3=paste(suff_fit, '_data', sep='')
+# 
+# plot_data_maps(y_veg, centers=centers_pls, taxa=taxa, ages, N_pls, K, T, thresh=0.5, limits, suff=suff3, save_plots=save_plots)
+# 
+# suff4=paste(suff_fit, '_data_binned', sep='')
+# plot_data_maps_binned(y_veg, centers=centers_pls, taxa=taxa, ages, N_pls, K, T, breaks, limits, suff=suff4, save_plots=save_plots)
+# 
+# 
+# # N_cores and centers_polU broken for split domain data....
+# idx.keep  = c(1,2,length(ages)/2,T)
+# # plot_core_locations(y, centers_polU, centers_pls, ages, limits)
+# plot_core_locations_select(y, centers_pol, centers_pls, ages, idx.keep, limits, fpat=subDir)
+# 
+# # # ####################################################################################################
+# # # # chunk: plot predicted and observed proportions in same frame
+# # # ####################################################################################################
+# # # suff4=paste(suff, '_compare', sep='')
+# # # 
+# # # plot_both_maps(r_mean, y_veg, centers=centers_pls, taxa=taxa, ages, N, K, T, thresh=1, suff=suff3, save_plots=save_plots)
