42bc265b540fa37556af9f98c9a9bb310f810456
Andria Dawson <andria.dawson@gmail.com>
2015-08-26 09:03:56 -0600

    runs.r update

--- r/runs.r
+++ r/runs.r
@@ -23,20 +23,20 @@ save_plots = TRUE
 #           suff_figs = 'G')
 
 
-# PL = list(suff_dat  = '12taxa_699cells_120knots_50to2050ypb_PL_umw_3by_v0.3_bacon',
-#           suff_fit  = '12taxa_699cells_120knots_50to2050ypb_PL_umw_3by_v3_bacon',
-#           suff_figs = 'PL')
-# 
-# G  = list(suff_dat  = '12taxa_699cells_120knots_50to2050ypb_G_umw_3by_v0.3_bacon', 
-#           suff_fit  = '12taxa_699cells_120knots_50to2050ypb_G_umw_3by_v3_bacon', 
-#           suff_figs = 'G')
+PL = list(suff_dat  = '12taxa_699cells_120knots_50to2050ypb_PL_umw_3by_v0.3_bacon',
+          suff_fit  = '12taxa_699cells_120knots_50to2050ypb_PL_umw_3by_v3_bacon',
+          suff_figs = 'PL')
 
-PL = list(suff_dat = '12taxa_699cells_120knots_50to2050ypb_KW_KGAMMA_PL_umw_3by_v0.3_bacon',
+G  = list(suff_dat  = '12taxa_699cells_120knots_50to2050ypb_G_umw_3by_v0.3_bacon', 
+          suff_fit  = '12taxa_699cells_120knots_50to2050ypb_G_umw_3by_v3_bacon', 
+          suff_figs = 'G')
+
+PL_var = list(suff_dat = '12taxa_699cells_120knots_50to2050ypb_KW_KGAMMA_PL_umw_3by_v0.3_bacon',
           suff_fit = '12taxa_699cells_120knots_50to2050ypb_KW_KGAMMA_PL_umw_3by_v3_bacon',
           suff_figs = 'KW_KGAMMA_PL')
 
-G  = list(suff_dat = '12taxa_699cells_120knots_50to2050ypb_KW_KGAMMA_G_umw_3by_v0.3_bacon', 
+G_var  = list(suff_dat = '12taxa_699cells_120knots_50to2050ypb_KW_KGAMMA_G_umw_3by_v0.3_bacon', 
           suff_fit = '12taxa_699cells_120knots_50to2050ypb_KW_KGAMMA_G_umw_3by_v3_bacon',
           suff_figs = 'KW_KGAMMA_G')
 
-runs = list(PL, G)
+runs = list(PL, G, PL_var, G_var)
