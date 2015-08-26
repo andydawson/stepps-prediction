c9c8ca0205865c74fed636d2105211051bad87a7
Andria Dawson <andria.dawson@gmail.com>
2015-08-25 18:49:03 -0600

    update runs.r

--- r/runs.r
+++ r/runs.r
@@ -23,12 +23,12 @@ save_plots = TRUE
 #           suff_figs = 'G')
 
 
-PL = list(suff_dat  = '12taxa_699cells_120knots_0to2000ypb_PL_umw_3by_v0.3',
-          suff_fit  = '12taxa_699cells_120knots_0to2000ypb_PL_umw_3by',
+PL = list(suff_dat  = '12taxa_699cells_120knots_0to2000ypb_PL_umw_3by_v0.3_bacon',
+          suff_fit  = '12taxa_699cells_120knots_0to2000ypb_PL_umw_3by_v3_bacon',
           suff_figs = 'PL')
 
-G  = list(suff_dat  = '12taxa_699cells_120knots_0to2000ypb_G_umw_3by_v0.3', 
-          suff_fit  = '12taxa_699cells_120knots_0to2000ypb_G_umw_3by', 
+G  = list(suff_dat  = '12taxa_699cells_120knots_50to2050ypb_G_umw_3by_v0.3_bacon', 
+          suff_fit  = '12taxa_699cells_120knots_50to2050ypb_G_umw_3by_v3_bacon', 
           suff_figs = 'G')
 
 # PL = list(suff_dat = '12taxa_699cells_120knots_0to2000ypb_KW_KGAMMA_PL_umw_3by_v0.3',
