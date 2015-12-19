2035a0dec701749e03b48b74f53c094e8e121bbd
Andria Dawson <andria.dawson@gmail.com>
2015-12-19 09:04:52 -0800

    Fix path issues.

--- scf-slurm.sh
+++ scf-slurm.sh
@@ -4,10 +4,10 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=andria.dawson@gmail.com
 #SBATCH --exclude=scf-sm20
-#SBATCH --error=$HOME/Documents/projects/stepps-prediction/runs/{model}/{run}/slurm-%j.err
-#SBATCH --output=$HOME/Documents/projects/stepps-prediction/runs/{model}/{run}/slurm-%j.out
+#SBATCH --workdir=/accounts/gen/vis/adawson/Documents/projects/stepps-prediction/runs/{model}/{run}
+#SBATCH --error="slurm-%j.err"
+#SBATCH --output="slurm-%j.out"
 
-cd $HOME/Documents/projects/stepps-prediction/runs/{model}/{run}
 cp $HOME/Documents/projects/stepps-prediction/cpp/{cpp} .
 cp $HOME/Documents/projects/stepps-prediction/cpp/{exe} .
 
